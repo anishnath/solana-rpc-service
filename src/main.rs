770e1c75cee574a483893e7833b77730b464cd44
anish <anish@noreply.com>
2023-03-07 02:51:53 +0530

    Initial commit adding cargo skelton

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use solana_client::{rpc_client::RpcClient, rpc_config::RpcSendTransactionConfig};
-use serde_json::{Result, Value};
+use std::env;
+use solana_client::rpc_client::RpcClient;
 
-fn main() -> Result<()> {
-    let rpc_client = RpcClient::new("https://api.mainnet-beta.solana.com".to_string());
+fn get_current_epoch(rpc_client: &RpcClient) -> u64 {
+    // Get the current epoch
+    let epoch_info = rpc_client.get_epoch_info().unwrap();
+    epoch_info.epoch
+}
 
-    let start_slot = 117894315;
-    let end_slot = 117894318;
-    let blocks: Vec<Value> = rpc_client
-        .get_blocks(start_slot, Some(end_slot))
-        .unwrap_or_else(|err| panic!("Failed to get blocks: {:?}", err));
+fn main() {
+    // Get the Solana network from the command-line argument
+    let args: Vec<String> = env::args().collect();
+    let network = args.get(1).unwrap_or(&"mainnet".to_string());
 
-    for block in blocks {
-        let blockhash = block["blockhash"].as_str().expect("Block hash not found");
-        println!("{}", blockhash);
-    }
+    // Choose a Solana network to connect to based on the command-line argument
+    let endpoint = match network.as_str() {
+        "devnet" => "https://api.devnet.solana.com".to_string(),
+        "testnet" => "https://api.testnet.solana.com".to_string(),
+        "mainnet" => "https://api.mainnet-beta.solana.com".to_string(),
+        _ => network.clone(), // Custom network
+    };
+    let rpc_client = RpcClient::new(endpoint);
 
-    Ok(())
+    let current_epoch = get_current_epoch(&rpc_client);
+    println!("Current epoch on {} is: {}", network, current_epoch);
 }
