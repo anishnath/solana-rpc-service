0740ade2ddac0633b844cf383ebdf45a3a7b0953
anish <anish@noreply.com>
2023-03-07 12:21:42 +0530

    web service error

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,92 @@
-use std::env;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use solana_client::rpc_client::RpcClient;
+use std::sync::Arc;
+use serde::{Deserialize, Serialize};
 
-fn create_rpc_client(network: &str) -> RpcClient {
-    // Choose a Solana network to connect to based on the command-line argument
+fn create_rpc_client(endpoint: String) -> RpcClient {
+    RpcClient::new(endpoint)
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct EpochInfoRequest {
+    network: Option<String>,
+}
+
+fn get_current_epoch(rpc_client: Arc<RpcClient>) -> u64 {
+    // Get the current epoch
+    let epoch_info = rpc_client.get_epoch_info().unwrap();
+    epoch_info.epoch
+}
+
+/**
+async fn get_epoch_info(network: web::Path<String>) -> HttpResponse {
+    // Choose a Solana network to connect to based on the request path
+    let endpoint = match network.as_str() {
+        "devnet" => "https://api.devnet.solana.com".to_string(),
+        "testnet" => "https://api.testnet.solana.com".to_string(),
+        "mainnet" => "https://api.mainnet-beta.solana.com".to_string(),
+        _ => network.to_string(), // Custom network
+    };
+
+    println!({},endpoint)
+    let rpc_client = Arc::new(create_rpc_client(endpoint));
+
+    // Get the current epoch
+    let current_epoch = get_current_epoch(rpc_client);
+
+    HttpResponse::Ok().json(current_epoch)
+}
+*/
+
+async fn get_epoch_info(info: web::Json<EpochInfoRequest>) -> HttpResponse {
+    // Get the network name from the request
+    let network = match &info.network {
+        Some(network) => network,
+        None => "mainnet", // Default network if not provided
+    };
+
+    // Choose a Solana network to connect to based on the request path
     let endpoint = match network {
         "devnet" => "https://api.devnet.solana.com".to_string(),
         "testnet" => "https://api.testnet.solana.com".to_string(),
         "mainnet" => "https://api.mainnet-beta.solana.com".to_string(),
         _ => network.to_string(), // Custom network
     };
-    RpcClient::new(endpoint)
-}
 
-fn get_current_epoch(network: &str) -> u64 {
-    // Create a RPC client for the specified network
-    let rpc_client = create_rpc_client(network);
+    println!("{}",endpoint);
+    let rpc_client = Arc::new(create_rpc_client(endpoint));
 
     // Get the current epoch
-    let epoch_info = rpc_client.get_epoch_info().unwrap();
-    epoch_info.epoch
+    let current_epoch = get_current_epoch(rpc_client);
+
+    println!("{}",current_epoch);
+
+    HttpResponse::Ok().json(current_epoch)
 }
 
-fn main() {
-    // Get the Solana network from the command-line argument
-    let args: Vec<String> = env::args().collect();
-    let network = args.get(1).unwrap_or(&"mainnet".to_string()).to_string();
+/**
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/{network}/epoch").route(web::get().to(get_epoch_info)))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+*/
 
-    // Get the current epoch for the specified network
-    let current_epoch = get_current_epoch(&network);
-    println!("Current epoch on {} is: {}", network, current_epoch);
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(
+                web::resource("/epoch")
+                    .route(web::post().to(get_epoch_info))
+            )
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
